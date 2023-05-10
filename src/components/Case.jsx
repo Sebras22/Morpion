b4d102821056a8bbbd03481c7b4d107e10cae271
Sebras22 <sebastien.branly@efrei.net>
2023-05-10 12:45:42 +0200

    2eme commit

--- src/components/Case.jsx
+++ src/components/Case.jsx
@@ -1,19 +1,11 @@
-import './App.css'
+import React from "react";
 
-
-
-
-
-  
-
-function Case({value}) {
-
-  return
-    <button className='case'>{value}</button>
-  
+function Case({ value, caseclick, disabled }) {
+    return (
+        <button className="case" disabled={disabled} onClick={caseclick}>
+            {value}
+        </button>
+    );
 }
-
-
-
 
 export default Case;
